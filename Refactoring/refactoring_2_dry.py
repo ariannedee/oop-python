937cf26879819e5d21aec36d31a7297e843b3b73
ariannedee <ariannedee@gmail.com>
2020-05-12 12:51:32 -0700

    Refactor the dry example

--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -3,29 +3,24 @@ Simulate a simple board game.
 There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
-
-Using procedural/imperative programming but refactored and abstracted
 """
 import random
 
-scores = [0, 0]
 
-print('Start game!')
+def play_game():
+    player_scores = [0, 0]
 
-while True:
-    for player_num in range(1, 3):
-        dice = random.randint(1, 6)
-        scores[player_num-1] += dice
-        new_score = scores[player_num - 1]
-        print(f'Player {player_num} rolls a {dice} (score: {new_score})')
-        if new_score >= 100:
-            print(f'Player {player_num} wins!')
-            break
-    else:
-        continue
-    break
+    while True:
+        for i in range(len(player_scores)):
+            player_number = i + 1
+            player_die = random.randint(1, 6)
+            player_scores[i] += player_die
+            score = player_scores[i]
+            print(f'Player {player_number} score: {score}')
+            if score >= 100:
+                print(f'Player {player_number} wins!')
+                return
 
-print()
-print('Final score:')
-print(f'Player 1: {scores[0]}')
-print(f'Player 2: {scores[1]}')
+
+if __name__ == '__main__':
+    play_game()
