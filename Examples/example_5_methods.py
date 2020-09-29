71f8235f5af06216d4c92208cb2d4e9a0da6b623
ariannedee <ariannedee@gmail.com>
2020-09-29 11:18:46 -0700

    Update bike example

--- Examples/example_5_methods.py
+++ Examples/example_5_methods.py
@@ -1,45 +1,56 @@
+"""
+Bike class for use in a retail shop
+"""
 from enum import Enum
 
 
 class Condition(Enum):
-    NEW = 1
-    GOOD = 0.8
-    OKAY = 0.5
-    BAD = 0.2
+    NEW = 0
+    GOOD = 1
+    OKAY = 2
+    BAD = 3
 
 
 class MethodNotAllowed(Exception):
     pass
 
 
-class Bike(object):
+class Bike:
     def __init__(self, description, condition, sale_price, cost=0):
-        self.description = description
-        self.condition = condition
-        self.sale_price = sale_price
         self.cost = cost
+        self.sale_price = sale_price
+        self.condition = condition
+        self.description = description
 
         self.sold = False
 
-    def update_sale_price(self, sale_price):
+    def update_sale_price(self, new_sale_price):
         if self.sold:
-            raise MethodNotAllowed('Bike has already been sold')
-        self.sale_price = sale_price
+            raise MethodNotAllowed("You can't update the sale price of a bike that's been sold")
+        self.sale_price = new_sale_price
 
     def sell(self):
-        """
-        Mark as sold and determine the profit received from selling the bike
-        """
         self.sold = True
         profit = self.sale_price - self.cost
         return profit
 
-    def service(self, spent, sale_price=None, condition=None):
-        """
-        Service the bike and update attributes
-        """
-        self.cost += spent
-        if sale_price:
-            self.update_sale_price(sale_price)
-        if self.condition:
-            self.condition = condition
+    def service(self, cost, new_sale_price=None, new_condition=None):
+        self.cost += cost
+        if new_sale_price:
+            self.update_sale_price(new_sale_price)
+        if new_condition:
+            self.condition = new_condition
+
+
+if __name__ == '__main__':
+    my_bike = Bike("Red Releigh cruiser", Condition.GOOD, 450, 50)
+    print(my_bike)
+
+    my_bike.update_sale_price(400)
+
+    profit = my_bike.sell()
+
+    print(profit)
+
+    my_bike.update_sale_price(1000)
+
