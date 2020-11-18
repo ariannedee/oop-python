a12dff1a6b1335e0e04a51dd274eed7d92b4402f
ariannedee <ariannedee@gmail.com>
2020-11-18 11:48:51 -0800

    Update bike example with dunder methods

--- bike_example.py
+++ bike_example.py
@@ -11,7 +11,11 @@ class Condition(Enum):
     BAD = 4
 
 
-class Bike:
+class MethodNotAllowed(Exception):
+    pass
+
+
+class Bike(object):
     def __init__(self, description, condition, sale_price, cost=0):
         self.cost = cost
         self.sale_price = sale_price
@@ -21,6 +25,8 @@ class Bike:
         self.sold = False
 
     def update_sale_price(self, new_sale_price):
+        if self.sold:
+            raise MethodNotAllowed("You cannot update the price of a sold bike")
         self.sale_price = new_sale_price
 
     def sell(self):
@@ -30,6 +36,12 @@ class Bike:
     def service(self, cost, new_sale_price):
         self.cost += cost
         self.update_sale_price(new_sale_price)
+
+    def __str__(self):
+        return f"{self.description}{' (sold)' if self.sold else ''}"
+
+    def __repr__(self):
+        return f"Bike: {self} {self.condition.name}"
 
 
 if __name__ == '__main__':
@@ -43,3 +55,4 @@ if __name__ == '__main__':
 
     # Determine profit
     print(profit)
+
