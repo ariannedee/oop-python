56f48f2be55465427800ff91ec9d046e06306aaf
ariannedee <ariannedee@gmail.com>
2020-11-18 13:41:51 -0800

    Update for current class

--- Refactoring/refactoring_5_inheritance/game.py
+++ Refactoring/refactoring_5_inheritance/game.py
@@ -2,23 +2,20 @@ from player import Player, LuckyPlayer
 
 
 class Game:
-    def __init__(self, num_players, win_score=100):
-        self.win_score = win_score
+    def __init__(self, num_players, target_score=100):
+        self.target_score = target_score
         self.players = []
-        for num in range(num_players):
-            if num == 0:
-                player = LuckyPlayer(num + 1)
-            else:
-                player = Player(num + 1)
-            self.players.append(player)
+        for i in range(num_players - 1):
+            self.players.append(Player(i + 1))
+        self.players.append(LuckyPlayer(num_players))
+
+    def player_won(self, player):
+        return player.score >= self.target_score
 
     def run_game(self):
         while True:
             for player in self.players:
                 player.take_turn()
-                if self.has_won(player):
+                if self.player_won(player):
                     print(f"{player} wins!")
                     return
-
-    def has_won(self, player):
-        return player.score >= self.win_score
--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -3,34 +3,33 @@ import random
 
 class Player:
     def __init__(self, player_num):
+        self.player_num = player_num
         self._score = 0
-        self._num = player_num
 
     @staticmethod
-    def _roll_die():
+    def roll_die():
         return random.randint(1, 6)
+
+    def take_turn(self):
+        roll = self.roll_die()
+        self._score += roll
+        print(f"{self} score: {self._score} (rolled a {roll})")
 
     @property
     def score(self):
         return self._score
 
-    @score.setter
-    def score(self, value):
-        self._score = value
-
-    def take_turn(self):
-        roll = self._roll_die()
-        self.score += roll
-        print(f"{self} rolled a {roll} ({self.score})")
-
     def __str__(self):
-        return f'Player {self._num}'
+        return f"Player {self.player_num}"
+
+    def __repr__(self):
+        return str(self)
 
 
 class LuckyPlayer(Player):
     @staticmethod
-    def _roll_die():
+    def roll_die():
         return random.randint(3, 6)
 
     def __str__(self):
-        return f'Player {self._num} (lucky)'
+        return f"{super().__str__()} (lucky)"
