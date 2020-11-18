128bb6df0b6aadc8d64a1eb596d9c013887ca034
ariannedee <ariannedee@gmail.com>
2020-11-18 12:53:16 -0800

    Update for current class

--- Refactoring/refactoring_3_abstraction.py
+++ Refactoring/refactoring_3_abstraction.py
@@ -8,39 +8,41 @@ import random
 
 
 class Player:
-    def __init__(self, player_number):
-        self.score = 0
-        self.player_number = player_number
+    def __init__(self, player_num):
+        self.player_num = player_num
+        self._score = 0
 
-    def roll_die(self):
-        die = random.randint(1, 6)
-        print(f'{self} rolled a {die}')
-        return die
+    @staticmethod
+    def roll_die():
+        return random.randint(1, 6)
 
-    def make_move(self):
-        self.score += self.roll_die()
-        print(f'{self}: {self.score}')
+    def take_turn(self):
+        roll = self.roll_die()
+        self._score += roll
+        print(f"{self} score: {self._score} (rolled a {roll})")
+
+    def has_won(self):
+        return self._score >= 100
 
     @property
-    def has_won(self):
-        return self.score >= 100
+    def score(self):
+        return self._score
 
     def __str__(self):
-        return f'Player {self.player_number}'
+        return f"Player {self.player_num}"
 
 
-def play_game(num_players=2):
+def play_game(num_players):
     players = []
     for i in range(num_players):
         players.append(Player(i + 1))
-
     while True:
         for player in players:
-            player.make_move()
-            if player.has_won:
-                print(f'{player} wins!')
+            player.take_turn()
+            if player.has_won():
+                print(f"{player} wins!")
                 return
 
 
 if __name__ == '__main__':
-    play_game(num_players=3)
+    play_game(3)
