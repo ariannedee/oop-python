1bfd9b8722eee66ec9057ff3078f9bf8922681f1
ariannedee <ariannedee@gmail.com>
2021-01-20 08:57:01 -0800

    Revert for new class

--- Examples/example_2_using_classes.py
+++ Examples/example_2_using_classes.py
@@ -5,7 +5,7 @@ my_bike = Bike(description='Univega Alpina, orange',
                sale_price=500,
                cost=100)
 
-my_bike.service(spent=30, sale_price=600)  # cost=$130, sale_price=$600
+my_bike.service(cost=30, new_sale_price=600)  # cost=$130, sale_price=$600
 
 print(my_bike.sale_price)                  # 600
 
