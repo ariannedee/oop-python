ad5f7402937a8490c50194b442f8309e7230c7c1
ariannedee <ariannedee@gmail.com>
2021-03-23 13:01:27 -0700

    Update solutions for class

--- Refactoring/refactoring_5_inheritance/game.py
+++ Refactoring/refactoring_5_inheritance/game.py
@@ -2,23 +2,41 @@ from player import Player, LuckyPlayer
 
 
 class Game:
-    def __init__(self, num_players=2, target_score=100):
+    counter = 0
+
+    def __init__(self, num_players, target_score=100):
         self.target_score = target_score
-        self.players = []
-        for i in range(num_players - 1):
-            self.players.append(Player(i + 1))
-        self.players.append(LuckyPlayer(num_players))
+        self.players = [Player(i + 1) for i in range(num_players)]  # List comprehension
+        self.players[0] = LuckyPlayer(1)
+        Game.counter += 1
+        self.game_num = Game.counter
+        print(self.players)
+
+    def __del__(self):
+        Game.counter -= 1
 
     def play_game(self):
+        self._game_start()
+        self._game_play()
+        self._game_end()
+
+    def _game_play(self):
         while True:
             for player in self.players:
                 player.take_turn()
-                if self.game_over():
+                if player.has_won(self.target_score):
+                    print(f"{player} wins")
                     return
 
-    def game_over(self):
-        for player in self.players:
-            if player.did_win_game(self.target_score):
-                print(f"{player} wins")
-                return True
-        return False
+    def _game_start(self):
+        print(f"{self} start")
+
+    def _game_end(self):
+        print(f"{self} is over\n")
+
+    def __str__(self):
+        return f"Game {self.game_num}"
+
+    def __repr__(self):
+        return f"Game {self.game_num}: {len(self.players)} players"
+
--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -1,33 +1,40 @@
 from random import randint
 
 
-class AsDictMixin:
-    def as_dict(self):
-        return {
-            'id': id(self),
-            'object': self
-        }
+class MethodNotAllowed(BaseException):
+    pass
 
 
-class Player(AsDictMixin):
-    def __init__(self, player_number):
-        self.score = 0
-        self.player_number = player_number
+class Player:
+    def __init__(self, player_num):
+        self._score = 0
+        self.player_num = player_num
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, new_score):
+        raise MethodNotAllowed()
 
     def take_turn(self):
         roll = self._roll_die()
-        self.score += roll
-        print(f"{self}: {self.score} (rolled a {roll})")
+        self._score += roll
+        print(f"{self}: {self._score} (rolled a {roll})")
 
     @staticmethod
     def _roll_die():
         return randint(1, 6)
 
-    def did_win_game(self, target_score):
-        return self.score >= target_score
+    def has_won(self, target_score):
+        return self._score >= target_score
 
     def __str__(self):
-        return f'Player {self.player_number}'
+        return f"Player {self.player_num}"
+
+    def __repr__(self):
+        return f"{type(self).__name__} {self.player_num}: {self._score}"
 
 
 class LuckyPlayer(Player):
@@ -36,4 +43,5 @@ class LuckyPlayer(Player):
         return randint(3, 6)
 
     def __str__(self):
-        return f'Player {self.player_number} (lucky)'
+        player_str = super().__str__()
+        return player_str + " (lucky)"
