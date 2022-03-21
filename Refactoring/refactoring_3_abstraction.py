c26d35594a10582a738cff221485cbb4c3d54272
ariannedee <ariannedee@gmail.com>
2022-03-21 13:04:20 -0700

    Update based on Mar 21, 2022 class

--- Refactoring/refactoring_3_abstraction.py
+++ Refactoring/refactoring_3_abstraction.py
@@ -4,47 +4,38 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-from random import randint
+import random
 
 
 class Player:
-    def __init__(self, player_num):
+    def __init__(self, num):
         self.score = 0
-        self.num = player_num
+        self.num = num
 
-    @staticmethod
-    def _roll():
-        return randint(1, 6)
-
-    def make_move(self):
-        roll = self._roll()
+    def take_turn(self):
+        roll = random.randint(1, 6)
         self.score += roll
-        print(f"{self} score: {self.score} (rolled a {roll})")
+        print(f'{self} rolled a {roll} ({self.score})')
 
     def has_won(self):
         return self.score >= 100
 
     def __str__(self):
-        return f"Player {self.num}"
+        return f'Player {self.num}'
 
     def __repr__(self):
-        return f"Player({self.num})"
+        return f'Player({self.num})'
 
 
 def run_game(num_players=2):
     players = [Player(i + 1) for i in range(num_players)]
-
     while True:
         for player in players:
-            player.make_move()
-
+            player.take_turn()
             if player.has_won():
-                print(f"{player} wins!")
+                print(f'{player} wins!')
                 return
 
 
 if __name__ == '__main__':
-    print("------ GAME 1 --------")
-    run_game(4)
-    print("------ GAME 2 --------")
-    run_game(3)
+    run_game(num_players=3)
