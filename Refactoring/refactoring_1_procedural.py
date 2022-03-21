b0518080dc849bbad531fc4710c0358315eec50e
ariannedee <ariannedee@gmail.com>
2022-03-21 11:36:12 -0700

    Update based on Mar 21, 2022 class

--- Refactoring/refactoring_1_procedural.py
+++ Refactoring/refactoring_1_procedural.py
@@ -4,7 +4,7 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-from random import randint
+import random
 
 
 def run_game():
@@ -12,25 +12,19 @@ def run_game():
     player_2_score = 0
 
     while True:
-        player_1_roll = randint(1, 6)
+        player_1_roll = random.randint(1, 6)
         player_1_score += player_1_roll
-        print(f"Player 1 score: {player_1_score} (rolled a {player_1_roll})")
-
+        print(f'Player 1 rolled a {player_1_roll} ({player_1_score})')
         if player_1_score >= 100:
-            print("Player 1 wins!")
+            print('Player 1 wins!')
             return
-
-        player_2_roll = randint(1, 6)
+        player_2_roll = random.randint(1, 6)
         player_2_score += player_2_roll
-        print(f"Player 2 score: {player_2_score} (rolled a {player_2_roll})")
-
+        print(f'Player 2 rolled a {player_2_roll} ({player_2_score})')
         if player_2_score >= 100:
-            print("Player 2 wins!")
+            print('Player 2 wins!')
             return
 
 
 if __name__ == '__main__':
-    print("------ GAME 1 --------")
-    run_game()
-    print("------ GAME 2 --------")
     run_game()
--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -4,27 +4,23 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-from random import randint
+import random
 
 
-def run_game(num_players=2):
-    scores = [0 for _ in range(num_players)]
+def run_game():
+    scores = [0, 0]
 
     while True:
         for i, score in enumerate(scores):
             player_num = i + 1
-            roll = randint(1, 6)
+            roll = random.randint(1, 6)
             score += roll
             scores[i] = score
-            print(f"Player {player_num} score: {score} (rolled a {roll})")
-
+            print(f'Player {player_num} rolled a {roll} ({score})')
             if score >= 100:
-                print(f"Player {player_num} wins!")
+                print(f'Player {player_num} wins!')
                 return
 
 
 if __name__ == '__main__':
-    print("------ GAME 1 --------")
-    run_game(4)
-    print("------ GAME 2 --------")
-    run_game(3)
+    run_game()
