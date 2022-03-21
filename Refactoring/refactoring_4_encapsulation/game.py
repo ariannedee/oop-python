94252bf68f6addf796485d635afab3f250163677
ariannedee <ariannedee@gmail.com>
2022-03-21 12:26:10 -0700

    Update based on Mar 21, 2022 class

--- Refactoring/refactoring_4_encapsulation/game.py
+++ Refactoring/refactoring_4_encapsulation/game.py
@@ -5,28 +5,45 @@ class Game:
     counter = 0
 
     def __init__(self, num_players, target_score=100):
-        Game.counter += 1
-        self.game_num = Game.counter
-        self.players = [Player(i + 1) for i in range(num_players)]
         self.target_score = target_score
+        self.players = [Player(i + 1) for i in range(num_players)]
+        Game.counter += 1
+        self.num = Game.counter
 
-    def player_has_won(self, player):
-        return player.score >= self.target_score
+    def start(self):
+        print(f'---- {self} start ----')
+
+    def end(self):
+        print(f'---- {self} end ----')
 
     def run(self):
-        print(f"------ {self} start --------")
+        self.start()
+        self.take_turns()
+        self.end()
 
+    def take_turns(self):
         while True:
             for player in self.players:
-                player.make_move()
+                player.take_turn()
+            if self.game_over():
+                print(f'{self.winner} wins!')
+                return
 
-                if self.player_has_won(player):
-                    print(f"{player} wins!")
-                    print(f"------ {self} end --------")
-                    return
+    def game_over(self):
+        for player in self.players:
+            if player.score >= self.target_score:
+                return True
+        return False
+
+    @property
+    def winner(self):
+        max_score = -1
+        max_player = None
+        for player in self.players:
+            if player.score > max_score:
+                max_score = player.score
+                max_player = player
+        return max_player
 
     def __str__(self):
-        return f"Game {self.game_num}"
-
-    def __repr__(self):
-        return f"Game({len(self.players)})"
+        return f'Game {self.num}'
--- Refactoring/refactoring_4_encapsulation/main.py
+++ Refactoring/refactoring_4_encapsulation/main.py
@@ -4,11 +4,11 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-from game import Game  # In PyCharm, mark parent directory as Sources Root for imports to work
+from game import Game
 
 if __name__ == '__main__':
-    game_1 = Game(num_players=4, target_score=20)
-    game_2 = Game(num_players=3)
-
-    game_2.run()
-    game_1.run()
+    game1 = Game(num_players=2, target_score=50)
+    game1.run()
+    print()
+    game2 = Game(num_players=3, target_score=30)
+    game2.run()
