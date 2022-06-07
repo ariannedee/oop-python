8b3cad488509c78699989684b6e9bda2c7b0ce03
ariannedee <ariannedee@gmail.com>
2022-06-07 12:05:39 -0700

    Update for June 7, 2022 class

--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -4,23 +4,29 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-import random
+from random import randint
 
 
-def run_game():
-    scores = [0, 0]
+def play_game(num_players=2):
+    scores = [0 for _ in range(num_players)]
 
     while True:
         for i, score in enumerate(scores):
             player_num = i + 1
-            roll = random.randint(1, 6)
+            roll = randint(1, 6)
             score += roll
             scores[i] = score
             print(f'Player {player_num} rolled a {roll} ({score})')
+
             if score >= 100:
                 print(f'Player {player_num} wins!')
                 return
 
 
 if __name__ == '__main__':
-    run_game()
+    print("-- Game 1 start --")
+    play_game(num_players=3)
+    print("-- Game 1 end --")
+    print("-- Game 2 start --")
+    play_game(num_players=4)
+    print("-- Game 2 end --")
