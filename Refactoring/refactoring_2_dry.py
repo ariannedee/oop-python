2046800d5424b500f783ec1d4bbadd4ac3b89510
ariannedee <ariannedee@gmail.com>
2022-10-26 11:52:01 -0700

    Update examples from class

--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -7,28 +7,22 @@ The first person to space 100 wins.
 from random import randint
 
 
-def run_game(num_players=2):
-    # scores = []
-    # for _ in range(num_players):
-    #     scores.append(0)
-
-    scores = [0 for _ in range(num_players)]  # as list comprehension
-
-    print("------ START GAME -------")
+def run_game(num_players, target_score=100):
+    scores = [0 for _ in range(num_players)]
+    print("--- GAME START ---")
     while True:
         for i, score in enumerate(scores):
             player_num = i + 1
             roll = randint(1, 6)
             score += roll
             scores[i] = score
-            print(f'Player {player_num} rolled a {roll} ({score})')
-
-            if score >= 100:
-                print(f'Player {player_num} wins!')
-                print("------ END GAME -------")
+            print(f"Player {player_num} rolled a {roll} (total: {score})")
+            if score >= target_score:
+                print(f"Player {player_num} wins!")
+                print("--- GAME END ---\n")
                 return
 
 
 if __name__ == '__main__':
     run_game(2)
-    run_game(3)
+    run_game(3, target_score=40)
