be66010271d6478ae522c787efd83177883d4830
ariannedee <ariannedee@gmail.com>
2022-12-20 12:49:14 -0800

    Update for Dec 20, 2022 class

--- Refactoring/refactoring_4_encapsulation/player.py
+++ Refactoring/refactoring_4_encapsulation/player.py
@@ -2,21 +2,18 @@ from random import randint
 
 
 class Player:
-    def __init__(self, number):
+    def __init__(self, num):
+        self.num = num
         self._score = 0
-        self.number = number
+
+    def take_turn(self):
+        roll = randint(1, 6)
+        self._score += roll
+        print(f"{self}: {self._score} (rolled a {roll})")
 
     @property
     def score(self):
         return self._score
 
-    def take_turn(self):
-        roll = randint(1, 6)
-        self._score += roll
-        print(f"{self} rolled a {roll} (total: {self.score})")
-
     def __str__(self):
-        return f"Player {self.number}"
-
-    def __repr__(self):
-        return f"Player({self.number})"
+        return f"Player {self.num}"
