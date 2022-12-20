2bc8b92574f97a8571abec92d2d8daa5b48959e9
ariannedee <ariannedee@gmail.com>
2022-12-20 12:24:42 -0800

    Update for Dec 20, 2022 class

--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -6,23 +6,28 @@ The first person to space 100 wins.
 """
 from random import randint
 
+game = 0
+
 
 def run_game(num_players, target_score=100):
+    global game
+    game += 1
     scores = [0 for _ in range(num_players)]
-    print("--- GAME START ---")
+    print(f"----- GAME {game} START -----")
+
     while True:
         for i, score in enumerate(scores):
             player_num = i + 1
             roll = randint(1, 6)
             score += roll
             scores[i] = score
-            print(f"Player {player_num} rolled a {roll} (total: {score})")
+            print(f"Player {player_num}: {score} (rolled a {roll})")
             if score >= target_score:
                 print(f"Player {player_num} wins!")
-                print("--- GAME END ---\n")
+                print(f"----- GAME {game} END -----")
                 return
 
 
 if __name__ == '__main__':
-    run_game(2)
-    run_game(3, target_score=40)
+    run_game(3, 50)
+    run_game(4, 60)
