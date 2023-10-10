78b1eb67271ec5fc5661eb77099bc932092fa798
ariannedee <ariannedee@gmail.com>
2023-10-10 12:17:07 -0700

    Update solutions for Oct 10, 2023 class

--- Refactoring/refactoring_3_abstraction.py
+++ Refactoring/refactoring_3_abstraction.py
@@ -8,42 +8,34 @@ from random import randint
 
 
 class Player:
-    def __init__(self, player_num, score=0):
-        self.num = player_num
-        self.score = score
+    def __init__(self, num):
+        self.num = num
+        self.score = 0
 
     def take_turn(self):
         roll = randint(1, 6)
         self.score += roll
         print(f"{self}: {self.score} (rolled a {roll})")
 
-    def has_won(self, target_score):
-        return self.score >= target_score
-
     def __str__(self):
         return f"Player {self.num}"
 
-    def __repr__(self):
-        return f"Player({self.num}, {self.score})"
 
-
-def run_game(num_players=2, target_score=100):
+def run_game(num_players=2):
     players = [Player(i + 1) for i in range(num_players)]
 
     while True:
         for player in players:
             player.take_turn()
-
-            if player.has_won(target_score):
+            if player.score >= 100:
                 print(f"{player} wins!")
                 return
 
 
 if __name__ == '__main__':
-    print("---- GAME 1 START ----")
-    run_game(num_players=4, target_score=20)
-    print("---- GAME 1 END ----")
-    print()
-    print("---- GAME 2 START ----")
-    run_game(num_players=3, target_score=50)
-    print("---- GAME 2 END ----")
+    print("=== GAME 1 START ===")
+    run_game(num_players=2)
+    print("=== GAME 1 END ===")
+    print("=== GAME 2 START ===")
+    run_game(num_players=3)
+    print("=== GAME 2 END ===")
