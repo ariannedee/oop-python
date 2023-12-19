392c807e9c6294e8d05daff886910b9a2cee8074
ariannedee <ariannedee@gmail.com>
2023-12-19 12:53:11 -0800

    Update for Dec 19, 2023 class

--- Refactoring/refactoring_5_inheritance/main.py
+++ Refactoring/refactoring_5_inheritance/main.py
@@ -4,11 +4,11 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
+
 from game import Game
 
 if __name__ == '__main__':
-    game_1 = Game(num_players=2, target_score=50)
-    game_2 = Game(num_players=3, target_score=30)
-
-    game_1.run_game()
-    game_2.run_game()
+    game_1 = Game(num_players=3)
+    game_1.run()
+    game_2 = Game(num_players=2, target_score=40)
+    game_2.run()
--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -5,18 +5,20 @@ class Player:
     def __init__(self, num):
         self.num = num
         self._score = 0
+        self.__private = True
 
     @property
     def score(self):
         return self._score
 
+    def do_turn(self):
+        roll = self._roll()
+        self._score += roll
+        print(f"{self}: {self.score} (rolled a {roll})")
+
     @staticmethod
     def _roll():
         return randint(1, 6)
-
-    def take_turn(self):
-        self._score += self._roll()
-        print(f"{self}: {self._score} (rolled a {self._roll()})")
 
     def __str__(self):
         return f"Player {self.num}"
@@ -28,11 +30,11 @@ class LuckyPlayer(Player):
         return randint(3, 6)
 
     def __str__(self):
-        original = super().__str__()
-        return original + "*"
+        original_string = super().__str__()
+        return original_string + "*"
 
 
 def get_player(num):
-    if num % 2 == 0:
-        return LuckyPlayer(num)
-    return Player(num)
+    if num % 2 == 1:
+        return Player(num)
+    return LuckyPlayer(num)
