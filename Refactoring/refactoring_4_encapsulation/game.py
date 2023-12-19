ad8ee3f99602e4bc159d132ca08cd9254156ac31
ariannedee <ariannedee@gmail.com>
2023-12-19 12:38:04 -0800

    Update for Dec 19, 2023 class

--- Refactoring/refactoring_4_encapsulation/game.py
+++ Refactoring/refactoring_4_encapsulation/game.py
@@ -5,23 +5,35 @@ class Game:
     counter = 0
 
     def __init__(self, num_players=2, target_score=100):
-        self.target_score = target_score
         self.players = [Player(i + 1) for i in range(num_players)]
+        self.target_score = target_score
         Game.counter += 1
         self.num = Game.counter
 
-    def run_game(self):
-        print(f"=== {str(self).upper()} START ===")
-        self._take_turns()
-        print(f"=== {self} END ===".upper())
+    def game_over(self):
+        for player in self.players:
+            if player.score >= self.target_score:
+                return True
+        return False
 
-    def _take_turns(self):
+    def get_winner(self):
+        max_score = 0
+        max_player = None
+        for player in self.players:
+            if player.score >= max_score:
+                max_score = player.score
+                max_player = player
+        return max_player
+
+    def run(self):
+        print(f"---- {self} start ----".upper())
         while True:
             for player in self.players:
-                player.take_turn()
-                if player.score >= self.target_score:
-                    print(f"{player} wins!")
-                    return
+                player.do_turn()
+            if self.game_over():
+                print(f"{self.get_winner()} wins!")
+                print(f"---- {self} end ----".upper())
+                return
 
     def __str__(self):
         return f"Game {self.num}"
--- Refactoring/refactoring_4_encapsulation/player.py
+++ Refactoring/refactoring_4_encapsulation/player.py
@@ -10,10 +10,10 @@ class Player:
     def score(self):
         return self._score
 
-    def take_turn(self):
+    def do_turn(self):
         roll = randint(1, 6)
         self._score += roll
-        print(f"{self}: {self._score} (rolled a {roll})")
+        print(f"{self}: {self.score} (rolled a {roll})")
 
     def __str__(self):
         return f"Player {self.num}"
