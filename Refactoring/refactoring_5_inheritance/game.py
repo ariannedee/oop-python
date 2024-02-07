31131e30f57b47d7a6ba49b4434dc3682cb731bc
ariannedee <ariannedee@gmail.com>
2024-02-07 12:49:52 -0800

    Update class solution for Feb 2024

--- Refactoring/refactoring_5_inheritance/game.py
+++ Refactoring/refactoring_5_inheritance/game.py
@@ -5,34 +5,35 @@ class Game:
     counter = 0
 
     def __init__(self, num_players=2, target_score=100):
-        self.players = [get_player(i + 1) for i in range(num_players)]
         self.target_score = target_score
+        self.players = [get_player(i + 1) for i in range(num_players)]
         Game.counter += 1
         self.num = Game.counter
 
-    def game_over(self):
-        for player in self.players:
-            if player.score >= self.target_score:
-                return True
-        return False
-
-    def get_winner(self):
-        max_score = 0
-        max_player = None
-        for player in self.players:
-            if player.score >= max_score:
-                max_score = player.score
-                max_player = player
-        return max_player
-
     def run(self):
-        print(f"---- {self} start ----".upper())
+        print(f"---- {self} START ----".upper())
+        self._take_turns()
+        print(f"---- {self} END ----".upper())
+
+    def check_if_game_over(self):
+        game_over = False
+        winner = None
+        high_score = 0
+        for player in self.players:
+            if player.score > high_score:
+                winner = player
+                high_score = player.score
+            if player.score >= self.target_score:
+                game_over = True
+        return game_over, winner
+
+    def _take_turns(self):
         while True:
             for player in self.players:
-                player.do_turn()
-            if self.game_over():
-                print(f"{self.get_winner()} wins!")
-                print(f"---- {self} end ----".upper())
+                player.take_turn()
+            game_over, winner = self.check_if_game_over()
+            if game_over:
+                print(f"{winner} wins!")
                 return
 
     def __str__(self):
--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -2,19 +2,26 @@ from random import randint
 
 
 class Player:
+    ROLL_BONUS_1S = 10
+
     def __init__(self, num):
         self.num = num
         self._score = 0
-        self.__private = True
+        self.prev_roll = None
 
     @property
     def score(self):
         return self._score
 
-    def do_turn(self):
+    def take_turn(self):
         roll = self._roll()
         self._score += roll
-        print(f"{self}: {self.score} (rolled a {roll})")
+        if self.prev_roll == 1 and roll == 1:
+            self._score += self.ROLL_BONUS_1S
+            print(f"{self}: {self.score} (rolled a {roll}, +10 bonus)")
+        else:
+            print(f"{self}: {self.score} (rolled a {roll})")
+        self.prev_roll = roll
 
     @staticmethod
     def _roll():
@@ -23,6 +30,9 @@ class Player:
     def __str__(self):
         return f"Player {self.num}"
 
+    def __repr__(self):
+        return f"Player({self.num})"
+
 
 class LuckyPlayer(Player):
     @staticmethod
@@ -30,11 +40,10 @@ class LuckyPlayer(Player):
         return randint(3, 6)
 
     def __str__(self):
-        original_string = super().__str__()
-        return original_string + "*"
+        return super().__str__() + "*"
 
 
-def get_player(num):
-    if num % 2 == 1:
-        return Player(num)
-    return LuckyPlayer(num)
+def get_player(player_num):
+    if player_num == 2:
+        return LuckyPlayer(player_num)
+    return Player(player_num)
