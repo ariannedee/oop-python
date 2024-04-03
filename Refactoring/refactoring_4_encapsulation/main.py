8bdc380da73f94bf53646155253120683b209b4d
ariannedee <ariannedee@gmail.com>
2024-04-03 12:41:25 -0700

    Update class solution for Apr 2024

--- Refactoring/refactoring_4_encapsulation/main.py
+++ Refactoring/refactoring_4_encapsulation/main.py
@@ -4,11 +4,11 @@ There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
 The first person to space 100 wins.
 """
-
 from game import Game
 
 if __name__ == '__main__':
-    game_1 = Game(num_players=3)
-    game_1.run()
-    game_2 = Game(num_players=2, target_score=40)
-    game_2.run()
+    game_1 = Game(num_players=5, target_score=30)
+    game_1.start()
+    print()
+    game_2 = Game(num_players=3, target_score=60)
+    game_2.start()
--- Refactoring/refactoring_4_encapsulation/player.py
+++ Refactoring/refactoring_4_encapsulation/player.py
@@ -2,29 +2,21 @@ from random import randint
 
 
 class Player:
-    ROLL_BONUS_1S = 10
-
     def __init__(self, num):
-        self.num = num
         self._score = 0
-        self.prev_roll = None
+        self.num = num
+
+    def take_turn(self):
+        roll = randint(1, 6)
+        self._score += roll
+        print(f"{self}: {self._score} (rolled a {roll})")
 
     @property
     def score(self):
         return self._score
 
-    def take_turn(self):
-        roll = randint(1, 6)
-        self._score += roll
-        if self.prev_roll == 1 and roll == 1:
-            self._score += self.ROLL_BONUS_1S
-            print(f"{self}: {self.score} (rolled a {roll}, +10 bonus)")
-        else:
-            print(f"{self}: {self.score} (rolled a {roll})")
-        self.prev_roll = roll
-
     def __str__(self):
         return f"Player {self.num}"
 
     def __repr__(self):
-        return f"Player({self.num})"
+        return f"{type(self).__name__}({self.num}, score={self._score})"
