cef36582438b80f210b482f8f6cde0aed604745d
ariannedee <ariannedee@gmail.com>
2024-06-25 12:37:32 -0700

    Update class solution for June 2024

--- Refactoring/refactoring_5_inheritance/main.py
+++ Refactoring/refactoring_5_inheritance/main.py
@@ -7,8 +7,7 @@ The first person to space 100 wins.
 from game import Game
 
 if __name__ == '__main__':
-    game_1 = Game(num_players=5, target_score=30)
-    game_1.start()
-    print()
-    game_2 = Game(num_players=3, target_score=60)
-    game_2.start()
+    game_1 = Game(num_players=3, target_score=50)
+    game_1.run()
+    game_2 = Game(num_players=4, target_score=70)
+    game_2.run()
--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -2,13 +2,14 @@ from random import randint
 
 
 class Player:
-    def __init__(self, num):
-        self._score = 0
-        self.num = num
+    def __init__(self, player_num, score=0):
+        self.num = player_num
+        self._score = score
 
     def take_turn(self):
-        self._score += self._roll()
-        print(f"{self}: {self._score} (rolled a {self._roll()})")
+        roll = self._roll()
+        self._score += roll
+        print(f"{self}: {self._score} (rolled a {roll})")
 
     @staticmethod
     def _roll():
@@ -19,7 +20,7 @@ class Player:
         return self._score
 
     def __str__(self):
-        return f"{type(self).__name__} {self.num}"
+        return f"Player {self.num}"
 
     def __repr__(self):
         return f"{type(self).__name__}({self.num}, score={self._score})"
@@ -30,8 +31,11 @@ class LuckyPlayer(Player):
     def _roll():
         return randint(3, 6)
 
+    def __str__(self):
+        return f"({super().__str__()})"
+
 
 def get_player(num):
-    if num == 2:
+    if num == 3:
         return LuckyPlayer(num)
     return Player(num)
