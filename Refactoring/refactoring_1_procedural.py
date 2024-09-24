ea0e64fd523191ac92810aae7cb01d650d61ed4d
ariannedee <ariannedee@gmail.com>
2024-09-24 11:55:44 -0700

    Update for Sep 2024 class

--- Refactoring/refactoring_1_procedural.py
+++ Refactoring/refactoring_1_procedural.py
@@ -6,26 +6,22 @@ The first person to space 100 wins.
 """
 from random import randint
 
+player_1_score = 0
+player_2_score = 0
 
-def run_game():
-    player_1_score = 0
-    player_2_score = 0
+while True:
+    player_1_roll = randint(1, 6)
+    player_1_score += player_1_roll
+    print(f"Player 1: {player_1_score} (rolled a {player_1_roll})")
 
-    while True:
-        player_1_roll = randint(1, 6)
-        player_1_score += player_1_roll
-        print(f"Player 1: {player_1_score} (rolled a {player_1_roll})")
-        if player_1_score >= 100:
-            print("Player 1 wins!")
-            return
+    if player_1_score >= 100:
+        print("Player 1 wins!")
+        break
 
-        player_2_roll = randint(1, 6)
-        player_2_score += player_2_roll
-        print(f"Player 2: {player_2_score} (rolled a {player_2_roll})")
-        if player_2_score >= 100:
-            print("Player 2 wins!")
-            return
+    player_2_roll = randint(1, 6)
+    player_2_score += player_2_roll
+    print(f"Player 2: {player_2_score} (rolled a {player_2_roll})")
 
-
-if __name__ == '__main__':
-    run_game()
+    if player_2_score >= 100:
+        print("Player 2 wins!")
+        break
