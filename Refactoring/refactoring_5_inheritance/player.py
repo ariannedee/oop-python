be7b3586b257e78264ec39a576354afec04cac04
ariannedee <ariannedee@gmail.com>
2024-09-25 11:45:24 -0700

    Update for Sep 2024 class

--- Refactoring/refactoring_5_inheritance/player.py
+++ Refactoring/refactoring_5_inheritance/player.py
@@ -2,28 +2,25 @@ from random import randint
 
 
 class Player:
-    def __init__(self, player_num, score=0):
+    def __init__(self, player_num):
         self.num = player_num
-        self._score = score
-
-    def take_turn(self):
-        roll = self._roll()
-        self._score += roll
-        print(f"{self}: {self._score} (rolled a {roll})")
-
-    @staticmethod
-    def _roll():
-        return randint(1, 6)
+        self._score = 0
 
     @property
     def score(self):
         return self._score
 
+    def take_turn(self):
+        roll = self._roll()
+        self._score += roll
+        print(f"{self}: {self.score} (rolled a {roll})")
+
+    @staticmethod
+    def _roll():
+        return randint(1, 6)
+
     def __str__(self):
         return f"Player {self.num}"
-
-    def __repr__(self):
-        return f"{type(self).__name__}({self.num}, score={self._score})"
 
 
 class LuckyPlayer(Player):
@@ -32,10 +29,11 @@ class LuckyPlayer(Player):
         return randint(3, 6)
 
     def __str__(self):
-        return f"({super().__str__()})"
+        base_str = super(Player, self).__str__()
+        return f"${base_str}$"
 
 
 def get_player(num):
-    if num == 3:
+    if num == 2:
         return LuckyPlayer(num)
     return Player(num)
