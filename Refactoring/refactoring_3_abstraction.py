1cd6281e2c58944d5f9ee3a5aee46676972efd2f
ariannedee <ariannedee@gmail.com>
2024-11-14 12:01:18 -0800

    Update for Nov 2024 class

--- Refactoring/refactoring_3_abstraction.py
+++ Refactoring/refactoring_3_abstraction.py
@@ -8,35 +8,39 @@ from random import randint
 
 
 class Player:
-    def __init__(self, player_num):
-        self.num = player_num
-        self.score = 0
+    def __init__(self, num, score=0):
+        self.num = num
+        self.score = score
 
     def take_turn(self):
         roll = randint(1, 6)
         self.score += roll
-        print(f"Player {self.num}: {self.score} (rolled a {roll})")
+        print(f"{self}: {self.score} (rolled a {roll})")
 
     def has_won(self, target_score):
         return self.score >= target_score
 
+    def __str__(self):
+        return f"Player {self.num}"
+
+    def __repr__(self):
+        return f"Player({self.num}, score={self.score})"
+
 
 def play_game(num_players=2, target_score=100):
     players = [Player(i + 1) for i in range(num_players)]
-
     while True:
         for player in players:
             player.take_turn()
-
             if player.has_won(target_score):
-                print(f"Player {player.num} wins!")
+                print(f"{player} wins!")
                 return
 
 
 if __name__ == '__main__':
-    print("--- GAME 1 START ---")
-    play_game(num_players=4, target_score=50)
-    print("--- GAME 1 END ---")
-    print("--- GAME 2 START ---")
-    play_game(num_players=3, target_score=30)
-    print("--- GAME 2 END ---")
+    print("---- GAME 1 start ----")
+    play_game(num_players=3, target_score=50)
+    print("---- GAME 1 end ----")
+    print("---- GAME 2 start ----")
+    play_game(num_players=4, target_score=40)
+    print("---- GAME 2 end ----")
