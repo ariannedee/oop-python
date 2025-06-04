84be8a037bd93d1cc3c63dcef07ec8df14d7a50d
ariannedee <ariannedee@gmail.com>
2025-06-04 12:24:43 -0700

    Update for June 2025 class

--- Refactoring/refactoring_3_abstraction.py
+++ Refactoring/refactoring_3_abstraction.py
@@ -6,7 +6,6 @@ The first person to space 100 wins.
 """
 from random import randint
 
-
 class Player:
     def __init__(self, num, score=0):
         self.num = num
@@ -24,26 +23,26 @@ class Player:
         return f"Player {self.num}"
 
     def __repr__(self):
-        return f"Player({self.num}, score={self.score})"
+        return f"Player({self.num}, {self.score})"
 
 
 def run_game(num_players=2, target_score=100):
-    players = []
+    players: list[Player] = []
     for i in range(num_players):
         players.append(Player(i + 1))
+
     while True:
         for player in players:
             player.take_turn()
-
             if player.has_won(target_score):
                 print(f"{player} wins!")
                 return
 
-
 if __name__ == '__main__':
-    print("----- GAME 1 START -----")
-    run_game(num_players=3, target_score=30)
-    print("----- GAME 1 END -----")
-    print("----- GAME 2 START -----")
-    run_game(num_players=4, target_score=50)
-    print("----- GAME 2 END -----")
+    print("---- GAME 1 START ----")
+    run_game(num_players=3, target_score=50)
+    print("---- GAME 1 END ----")
+    print()
+    print("---- GAME 2 START ----")
+    run_game(num_players=4, target_score=60)
+    print("---- GAME 2 END ----")
