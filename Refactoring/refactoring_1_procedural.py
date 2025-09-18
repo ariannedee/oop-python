8d0efcfa251a256b05e02495f10a00ee94389c5f
ariannedee <ariannedee@gmail.com>
2025-09-18 11:58:24 -0700

    Update for Sept 2025 class

--- Refactoring/refactoring_1_procedural.py
+++ Refactoring/refactoring_1_procedural.py
@@ -2,12 +2,11 @@
 Simulate a simple board game.
 There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
-The first person to space 100 wins.
+The first person to space 50 wins.
 """
 from random import randint
 
-
-def run_game():
+if __name__ == '__main__':
     player_1_score = 0
     player_2_score = 0
 
@@ -15,17 +14,15 @@ def run_game():
         player_1_roll = randint(1, 6)
         player_1_score += player_1_roll
         print(f"Player 1: {player_1_score} (rolled a {player_1_roll})")
-        if player_1_score >= 100:
+
+        if player_1_score >= 50:
             print("Player 1 wins!")
-            return
+            break
 
         player_2_roll = randint(1, 6)
         player_2_score += player_2_roll
         print(f"Player 2: {player_2_score} (rolled a {player_2_roll})")
-        if player_2_score >= 100:
+
+        if player_2_score >= 50:
             print("Player 2 wins!")
-            return
-
-
-if __name__ == '__main__':
-    run_game()
+            break
