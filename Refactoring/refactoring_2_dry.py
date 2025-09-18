6c5bfba98a36381aa6d6121c6851a16e8a6fc1f0
ariannedee <ariannedee@gmail.com>
2025-09-18 12:07:22 -0700

    Update for Sept 2025 class

--- Refactoring/refactoring_2_dry.py
+++ Refactoring/refactoring_2_dry.py
@@ -2,30 +2,29 @@
 Simulate a simple board game.
 There are 2 players.
 Each player takes turn rolling a die and moving that number of spaces.
-The first person to space 100 wins.
+The first person to space 50 wins.
 """
 from random import randint
 
-
-def run_game(num_players=2, target_score=100):
-    scores = [0] * num_players
+def play_game(num_players=2, target_score=50):
+    scores = [0 for i in range(num_players)]
 
     while True:
         for i, score in enumerate(scores):
+            player_num = i + 1
             roll = randint(1, 6)
             score += roll
             scores[i] = score
-            print(f"Player {i + 1}: {score} (rolled a {roll})")
+            print(f"Player {player_num}: {score} (rolled a {roll})")
+
             if score >= target_score:
-                print(f"Player {i + 1} wins!")
+                print(f"Player {player_num} wins!")
                 return
 
-
 if __name__ == '__main__':
-    print("---- GAME 1 START ----")
-    run_game(num_players=3, target_score=50)
-    print("---- GAME 1 END ----")
-    print()
-    print("---- GAME 2 START ----")
-    run_game(num_players=4, target_score=60)
-    print("---- GAME 2 END ----")
+    print("------ GAME 1 START ------")
+    play_game(3, 40)
+    print("------ GAME 1 END ------")
+    print("------ GAME 2 START ------")
+    play_game(4, 30)
+    print("------ GAME 2 END ------")
